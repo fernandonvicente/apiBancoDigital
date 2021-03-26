8413c89197dd687d7f3a98f32e7eacf2e98c56fa
fernando <fernando.nvicente@gmail.com>
2021-03-26 12:38:26 -0300

    ajustando connection do teste e colocando a resposta da api

--- tests/Feature/ApiTest.php
+++ tests/Feature/ApiTest.php
@@ -16,21 +16,30 @@ class ApiTest extends TestCase
     {
       $response = $this->get('/api/saldo/6874');
 
-      $response->assertStatus(200);
+       $response->assertStatus(200)
+          ->assertJsonFragment([
+            'message' => 'Saldo'
+          ]);
 
     }
 
     public function testSacar()
     {
-        $response = $this->get('/api/sacar/6874/100');
+        $response = $this->get('/api/sacar/6874/712');
 
-        $response->assertStatus(200);
+        $response->assertStatus(200)
+         ->assertJsonFragment([
+            'message' => 'Saque realizado'
+          ]);
     }
 
     public function testDepositar()
     {
-        $response = $this->get('/api/depositar/6874/100');
+        $response = $this->get('/api/depositar/6874/77');
 
-        $response->assertStatus(200);
+        $response->assertStatus(200)
+          ->assertJsonFragment([
+            'message' => 'Desposito realizado'
+          ]);
     }
 }

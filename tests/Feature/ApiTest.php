9767d63e77b12ee46d84cd44653525b1d79e8d7d
fernando <fernando.nvicente@gmail.com>
2021-03-25 22:12:33 -0300

    primeira publicacao

---
+++ tests/Feature/ApiTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ApiTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testSaldo()
+    {
+      $response = $this->get('/api/saldo/6874');
+
+        $response->assertStatus(200);
+
+        //dump($response);
+ 
+         $response->seeJson([
+            'message' => 'Saldo',
+        ]);
+        //seeJson
+
+
+    }
+
+    public function testSacar()
+    {
+        $response = $this->get('/api/sacar/6874/100');
+
+        $response->assertStatus(200);
+    }
+
+    public function testDepositar()
+    {
+        $response = $this->get('/api/depositar/6874/100');
+
+        $response->assertStatus(200);
+    }
+}
